be64378e21031a005d13c5cd218a12679f788090
ajel28 <dolahski123@gmail.com>
2024-01-16 17:52:57 -0500

    added worlds will finish buttons when we make new worlds

--- Menu.gd
+++ Menu.gd
@@ -2,10 +2,10 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://MarioGame.tscn")
+	get_tree().change_scene_to_file("res://worlds.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://worlds.tscn")
+	get_tree().change_scene_to_file("res://options.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
