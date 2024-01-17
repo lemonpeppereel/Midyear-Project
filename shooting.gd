7a43a72938ef67b8f49a48ec14095288af817aab
ajel28 <dolahski123@gmail.com>
2024-01-16 21:13:19 -0500

    added dragonball world with movement
    added a new dbz world clickable through our world play menu and i added up and down movement because for animations not all of our worlds are going to be having objects comgin from the top but some from the sides like the blood cells we made

--- shooting.gd
+++ shooting.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 func _input(_event):
 	if Input.is_action_just_pressed("ui_up")==true:
+		print("hi")
 		var coin = coin_scene.instantiate()
 		coin.global_position = get_parent().global_position
 		get_tree().root.get_node("Game").add_child(coin)
