4ca84fc694a92f4a3b0e9489589e74750f7e8041
ajel28 <dolahski123@gmail.com>
2024-01-16 19:52:42 -0500

    ..rename organization

--- shooting.gd
+++ shooting.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var coin_scene: PackedScene
 
-func _input(event):
+func _input(_event):
 	if Input.is_action_just_pressed("ui_up")==true:
 		var coin = coin_scene.instantiate()
 		coin.global_position = get_parent().global_position
