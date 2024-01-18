06f8f24164b00bcb467fc53b27b506b1712e1fe1
ajel28 <dolahski123@gmail.com>
2024-01-18 12:00:21 -0500

    fixed animation for coin shoot

--- Coin.gd
+++ Coin.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 @export var speed = 400
-@onready var animationz = $AnimationPlayer
+@onready var animations = $Coinani
 func _physics_process(delta):
 	global_position.y += -speed * delta
-	#coinshot()
+	coinshot()
 	
-#func coinshot():
-	#if Input.is_action_just_pressed("ui_up"):
-		#animationz.play("coinanimation")
+func coinshot():
+	if Input.is_action_just_pressed("ui_up"):
+		animations.play("coinanimation")
