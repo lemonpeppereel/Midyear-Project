b8e83ca1a7732abf4d8352dcfb102a74cc905957
Erik543210 <erikturdiu@gmail.com>
2024-01-19 20:54:34 -0500

    added random world changing when down key pressed (need to figure out how to access score in other nodes)

--- Coin.gd
+++ Coin.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 @export var speed = 400
 @onready var animations = $Coinani
-
+@export var score = 0
+	
 func _physics_process(delta):
 	global_position.y += -speed * delta
 	coinshot()
@@ -13,3 +14,4 @@ func coinshot():
 		
 func _on_area_entered(area):
 	area.queue_free()
+	score += 10
--- Node2D.gd
+++ Node2D.gd
@@ -4,35 +4,38 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 @export var koopa_scene: PackedScene
 var screensize = Vector2(3200,1800)
+var random = RandomNumberGenerator.new()
 
 @onready var koopascene = preload("res://koopatroopa.tscn")
+@onready var coingame = preload("res://Coin.gd")
 
-
+func _process(delta):
+	if Input.is_action_pressed("ui_down"):
+		_change_world()
+		
 func _input(_event):
 		rng.randomize()
 		if rng.randi_range(0,15) == 5:
 			var koopa = koopa_scene.instantiate()
 			koopa.position.x = randi_range(0, screensize.x)
 			get_tree().root.get_node("MarioGame").add_child(koopa)
-		
-var random = RandomNumberGenerator.new()
+	
 func _change_world():
-	if Input.is_action_pressed("ui_down"):
-			random.randomize()
+	random.randomize()
 	if random.randi_range(0,7) == 0:
-			get_tree().change_scene_to_file("res://goku_game.tscn")
+		get_tree().change_scene_to_file("res://goku_game.tscn")
 	elif random.randi_range(0,7) == 1:
-			get_tree().change_scene_to_file("res://halo.tscn")
+		get_tree().change_scene_to_file("res://halo.tscn")
 	elif random.randi_range(0,7) == 2:
-			get_tree().change_scene_to_file("res://tmnt.tscn")
+		get_tree().change_scene_to_file("res://tmnt.tscn")
 	elif random.randi_range(0,7) == 3:
-			get_tree().change_scene_to_file("res://streetfighter.tscn")
+		get_tree().change_scene_to_file("res://streetfighter.tscn")
 	elif random.randi_range(0,7) == 4:
-			get_tree().change_scene_to_file("res://StarWars.tscn")
+		get_tree().change_scene_to_file("res://StarWars.tscn")
 	elif random.randi_range(0,7) == 5:
-			get_tree().change_scene_to_file("res://spiderman.tscn")
+		get_tree().change_scene_to_file("res://spiderman.tscn")
 	elif random.randi_range(0,7) == 6:
-			get_tree().change_scene_to_file("res://samurai.tscn")
+		get_tree().change_scene_to_file("res://samurai.tscn")
 				
 			
 		
