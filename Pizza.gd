c184fe215ebb83104e7b5a36868be8f7ed38a0d1
Erik543210 <erikturdiu@gmail.com>
2024-01-22 15:03:52 -0500

    minor

--- Pizza.gd
+++ Pizza.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var speed = 400
 	
 func _physics_process(delta):
-	global_position.y += speed * -delta
+	global_position.x += speed * delta
 		
 func _on_area_entered(area):
 	area.queue_free()
--- ShootingOrigintmnt.gd
+++ ShootingOrigintmnt.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 func _input(_event):
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("ui_right"):
 		var pizza = pizza_scene.instantiate()
 		pizza.global_position = get_parent().global_position - Vector2(0, 20)
 		get_tree().root.get_node("tmnt").add_child(pizza)
