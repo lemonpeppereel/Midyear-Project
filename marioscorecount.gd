0d7309b1f30a3436c90327284257820b20367fe1
Erik543210 <erikturdiu@gmail.com>
2024-01-25 15:16:13 -0500

    tried to fix score during class didnt work

--- marioscorecount.gd
+++ marioscorecount.gd
@@ -3,11 +3,9 @@ extends Label
 var value = 0
 
 func _ready():
-	text = "Score: " + str(value)
-
+	self.text = "Score: " + str(value)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# func updatescore():
-	# var myLabel = $mariolabel
-	# var myLabel = $Label
-	# myLabel.text=str("asdfasdf")
-	# text = "Score: " + str(value+10)
+func updatescore():
+	self.text = "Score: " + str(value)
+	print(value)
