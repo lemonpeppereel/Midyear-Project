de03f92584877b0909c0617672d003cf898f37df
ajel28 <dolahski123@gmail.com>
2024-01-30 15:02:34 -0500

    tried to add coppa troopa animation

--- Coin.gd
+++ Coin.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
 @export var speed = 400
-@onready var animations = $Coinani
+#@onready var animations3 = $Coinani
 var screensize = Vector2(1152,640)
 
 func _physics_process(delta):
 	global_position.y += -speed * delta
 	#coinshot()
 
-#func coinshot():
+#unc coinshot():
 	#if Input.is_action_just_pressed("ui_up"):
-		#animations.play("coinanimation")
+		#animations3.play("coinanimation")
 		
 func _on_area_entered(area):
 	if area is Koopatroopa:
--- koopatroopa.gd
+++ koopatroopa.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 class_name Koopa
-
+@onready var Animationsk = $koopafall
 @export var speed = 130
 
 func _physics_process(delta):
 	global_position.y += speed * delta
-
+	koopafall()
+	
+func koopafall():
+	if Input.is_action_just_pressed("ui_up"):
+		Animationsk.play("kfall")
+	
 func _on_area_entered(area):
 	if area is Coin:
 		area.queue_free()
